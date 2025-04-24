4e36891fe75f54646f3b603f9be097e85170e24f
Sivakumarsrinivasan <ssivakumar6242001@gmail.com>
2025-04-24 15:32:16 +0530

    changes in portfolio section

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,4 +10,22 @@ export class ContactComponent {
 constructor(){
 this.mail = 'sivakumar6242001@gmail.com'
 }
-}
+
+
+sendEmail(event: Event) {
+  // event.preventDefault();
+  //   emailjs.sendForm("service_e97jy66", 'template_pws7zfs', event.target as HTMLFormElement, 'tFhDUhAHcj9S8O2lI')
+  //     .then((response) => {
+  //       console.log('Email sent successfully!', response);
+  //       alert('Email sent successfully!');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error sending email:', error);
+  //       alert('Failed to send email.');
+  //     });
+  }
+  navigateto(){
+    window.open('mailto:ssivakumar.dev@gmail.com')
+  }
+  }
+  
