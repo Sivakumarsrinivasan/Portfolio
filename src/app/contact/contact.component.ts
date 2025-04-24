fd1eee111511c3a288c8f2b1f6a1b0566e906ca3
Sivakumarsrinivasan <ssivakumar6242001@gmail.com>
2025-04-24 23:59:52 +0530

    Added email js

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import emailjs from 'emailjs-com';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -13,16 +13,16 @@ this.mail = 'sivakumar6242001@gmail.com'
 
 
 sendEmail(event: Event) {
-  // event.preventDefault();
-  //   emailjs.sendForm("service_e97jy66", 'template_pws7zfs', event.target as HTMLFormElement, 'tFhDUhAHcj9S8O2lI')
-  //     .then((response) => {
-  //       console.log('Email sent successfully!', response);
-  //       alert('Email sent successfully!');
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error sending email:', error);
-  //       alert('Failed to send email.');
-  //     });
+  event.preventDefault();
+    emailjs.sendForm("service_e97jy66", 'template_pws7zfs', event.target as HTMLFormElement, 'tFhDUhAHcj9S8O2lI')
+      .then((response) => {
+        console.log('Email sent successfully!', response);
+        alert('Email sent successfully!');
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+        alert('Failed to send email.');
+      });
   }
   navigateto(){
     window.open('mailto:ssivakumar.dev@gmail.com')
